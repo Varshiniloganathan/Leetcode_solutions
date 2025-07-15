ff1ed939c387d76800abbe702ba0af73d287fe62
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-07-15 11:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (87.67%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public char[] reverse(char[] chars,int start,int end){
+        while(start<=end){
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+        return chars;
+
+    }
+    public String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        for(int i=0;i<n;i+=2*k){
+            int start = i;
+            int end = Math.min(i+k-1,n-1);
+            reverse(chars,start,end);
+        }
+        return new String(chars);
+        
+    }
+}
