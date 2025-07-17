92ce5ad0f9b6e2c1c24b458c37440ca448f2ac43
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-07-17 22:20:08 +0530

    Time: 2 ms (67.92%) | Memory: 41.3 MB (86.54%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,37 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-       /* char[] arr = s.toCharArray();
-        char[] arr1 = t.toCharArray();
-        if(arr1.length<arr.length) return false;
-        if(arr.length==0 || (arr.length==0 && arr1.length==0)) return true;
-        int i=0;
-        int j =0;
-        while(i<arr.length){
-            if(arr[i]==arr1[j]){
-                i++;
-                if(j<arr1.length-1){
-                    j++;
-                }
-                else if(i<arr.length-1 && j==arr1.length-1){
-                    return false;
-                }
-            }
-            else if(arr[i]!=arr1[j] && j<arr1.length-1){
-                j++;
-            }
-            else{
-                return false;
-            }
-        }
-        return true;*/
-        int i=0,j=0;
-        while(i<s.length()&&j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
-                i++;
-            }
+       int i = 0;
+       int j = 0;
+       if(s.length() == 0) return true;
+       if(s.length()>t.length()) return false;
+       while(i<s.length()&&j<t.length()){
+        if(s.charAt(i) != t.charAt(j)){
             j++;
         }
-        return i==s.length();
+        else{
+            i++;
+            j++;
+        }
+       }
+       return i>=s.length();
         
     }
 }
