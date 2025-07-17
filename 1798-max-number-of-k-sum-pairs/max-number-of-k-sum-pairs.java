98792f550ed1225bef08e8509d09da83ed61bdf2
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-07-17 22:39:48 +0530

    Time: 19 ms (59.26%) | Memory: 57.3 MB (50.72%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length-1;
+        int cnt = 0;
+        while(i<j){
+            if(nums[i]+nums[j] < k){
+                i++;
+            }
+            else if(nums[i]+nums[j]>k){
+                j--;
+            }
+            else{
+                cnt++;
+                i++;
+                j--;
+            }
+        }
+        return cnt;
+        
+        
+    }
+}
