2025bf07a0c7f52673a7416a91d0fd367a05f8e1
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-07-30 23:39:22 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (48.26%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,17 +1,29 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    public int func(int[] nums,int goal){
+        int l=0;
+        int r=0;
         int n = nums.length;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum == goal){
-                    cnt++;
-                }
+        if(goal<0) return 0;
+        int sum = 0;
+        int cnt = 0;
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
             }
+            if(sum<=goal){
+               cnt = cnt + (r-l+1);
+            }
+            r++;
+            
+
         }
         return cnt;
-
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return func(nums,goal) - func(nums,goal-1);
+        
+        
     }
 }
