ec0e800f98e21d04b2820b793ab5ed385a30cbd3
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-08-03 20:20:26 +0530

    Time: 12 ms (97.69%) | Memory: 43.2 MB (76.23%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -13,7 +13,7 @@ class Solution {
                 if(sum==target){
                     return sum;
                 }
-                if(sum>target){
+                else if(sum>target){
                     k--;
                 }
                 else{
