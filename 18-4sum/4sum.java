d89b6ae2bf830f44d84ecd94daa469695a1aca1c
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-08-03 20:48:33 +0530

    Time: 65 ms (11.89%) | Memory: 45.4 MB (14.50%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -4,7 +4,7 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            //if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n-2;j++){
                 //if(j>i+1 && nums[j]==nums[j-1]) continue;
                 int k = j+1;
