47aec3267409df11eee026f2b353bc3589c78ce9
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-08-09 18:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (63.31%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,27 @@
 class Solution {
-    public void func(int[] nums,List<List<Integer>> ans,List<Integer> ds,Map<Integer,Integer> map){
-        if(ds.size()==nums.length){
-            ans.add(new ArrayList<>(ds));
+    public void func(int ind,int[] nums,List<List<Integer>> ans){
+        if(ind == nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            ans.add(ds);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!map.containsKey(nums[i])){
-                ds.add(nums[i]);
-                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-                func(nums,ans,ds,map);
-                ds.remove(ds.size()-1);
-                map.remove(nums[i]);
-            }
+        for(int i=ind;i<nums.length;i++){
+            swap(i,ind,nums);
+            func(ind+1,nums,ans);
+            swap(i,ind,nums);
         }
+    }
+    public void swap(int i,int ind,int[] nums){
+        int temp = nums[ind];
+        nums[ind]=nums[i];
+        nums[i]=temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
-        func(nums,ans,new ArrayList<>(),map);
+        func(0,nums,ans);
         return ans;
-        
     }
 }
