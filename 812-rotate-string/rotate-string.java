d9df893d7b3fa2662be93d3dbaabdc9efb7e8ed9
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-08-18 12:18:10 +0530

    Time: 5 ms (7.92%) | Memory: 42.2 MB (10.27%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -4,10 +4,12 @@ class Solution {
             return false;
         }
         int n = goal.length();
-        for(int i=0;i<n;i++){
+        int i=0;
+        while(i<n){
             char last = goal.charAt(n-1);
             goal = last+goal.substring(0,n-1);
             if(s.equals(goal)) return true;
+            i++;
         }
         return false;
         
