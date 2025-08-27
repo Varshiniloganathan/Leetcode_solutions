64b7f4e5cea69dc4d6034055ebeff12b1716b329
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2025-08-27 23:15:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (51.13%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,33 +1,33 @@
 class Solution {
-    public void func(int i,String digits,Map<Character,String> phone,List<String> ans,StringBuilder sb){
-        if(i == digits.length()){
+    public void func(int i,String digits,Map<Character,String> map,StringBuilder sb,List<String> ans){
+        if(sb.length()==digits.length()){
             ans.add(sb.toString());
             return;
         }
-        char d = digits.charAt(i);
-        String s = phone.get(d);
-        for(char l:s.toCharArray()){
+        char digit = digits.charAt(i);
+        String comb = map.get(digit);
+        for(char l:comb.toCharArray()){
             sb.append(l);
-            func(i+1,digits,phone,ans,sb);
+            func(i+1,digits,map,sb,ans);
             sb.deleteCharAt(sb.length()-1);
-        }
 
+        }
     }
     public List<String> letterCombinations(String digits) {
-        Map<Character,String> phone = new HashMap<>();
-        phone.put('2',"abc");
-        phone.put('3',"def");
-        phone.put('4',"ghi");
-        phone.put('5',"jkl");
-        phone.put('6',"mno");
-        phone.put('7',"pqrs");
-        phone.put('8',"tuv");
-        phone.put('9',"wxyz");
-        List<String> ans = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        if(digits==null || digits.length()==0) return ans;
-        func(0,digits,phone,ans,sb);
-        return ans;
-        
+            Map<Character,String> map = new HashMap<>();
+            map.put('2',"abc");
+            map.put('3',"def");
+            map.put('4',"ghi");
+            map.put('5',"jkl");
+            map.put('6',"mno");
+            map.put('7',"pqrs");
+            map.put('8',"tuv");
+            map.put('9',"wxyz");
+            StringBuilder sb = new StringBuilder();
+            List<String> ans = new ArrayList<>();
+            if(digits.length()==0) return ans;
+            func(0,digits,map,sb,ans);
+            return ans;
+
     }
 }
