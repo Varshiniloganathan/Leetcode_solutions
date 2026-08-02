6ee1e36d4d7d3f932f4c76b4ec22699a40e80611
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-08-02 12:43:33 +0530

    Time: 11 ms (7.29%) | Memory: 47.1 MB (7.92%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,9 +2,6 @@ class Solution {
     public int recur(List<List<Integer>> tri, int i, int j, HashMap<String,Integer> dp) {
         if (i == tri.size() )
             return 0;
-        if(j<0 || j>=tri.get(i).size()){
-            return Integer.MAX_VALUE;
-        }
         String key = i + " " + j;
         if (dp.containsKey(key))
             return dp.get(key);
@@ -21,9 +18,6 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int r = triangle.size();
         HashMap<String,Integer> dp = new HashMap<>();
-        // for (int i = 0; i < r; i++) {
-        //     Arrays.fill(dp[i],-1);
-        // }
         return recur(triangle, 0, 0, dp);
 
     }
