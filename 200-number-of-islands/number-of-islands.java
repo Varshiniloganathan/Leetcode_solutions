0dffcf654160951b49aafe6af59ae29a5ddf9734
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-08-07 20:19:17 +0530

    Time: 42 ms (5.37%) | Memory: 60.5 MB (5.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,36 +1,40 @@
 class Solution {
-    public int dfs(int row, int col, char[][] grid,int[][] visited){
-        if(row <0 || col<0 || row>=grid.length || col>= grid[0].length){
+    public int dfs(int row, int col, char[][] grid, Set<String> visited) {
+       
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) {
             return 0;
         }
-        if(visited[row][col] == -1) return 0;
-        
-        if(visited[row][col] != -1 && grid[row][col] != '0'){
-            visited[row][col] = -1;
-            dfs(row+1, col, grid, visited);
-            dfs(row-1, col, grid, visited);
-            dfs(row, col+1, grid, visited);
-            dfs(row, col-1, grid, visited);
+         String pos = row + " " + col;
 
-        }
+        if (grid[row][col] == '0')
+            return 0;
+        if (visited.contains(pos))
+            return 0;
+
+        visited.add(pos);
+        dfs(row + 1, col, grid, visited);
+        dfs(row - 1, col, grid, visited);
+        dfs(row, col + 1, grid, visited);
+        dfs(row, col - 1, grid, visited);
+
         return 1;
 
-
     }
+
     public int numIslands(char[][] grid) {
         int m = grid.length;
-        System.out.println(m);
         int n = grid[0].length;
-        int[][] visited = new int[m][n];
+        // int[][] visited = new int[m][n];
+        Set<String> visited = new HashSet<>();
         int ans = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]!= '0'){
-                    ans+= dfs(i, j, grid, visited);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != '0') {
+                    ans += dfs(i, j, grid, visited);
                 }
             }
         }
         return ans;
-        
+
     }
 }
