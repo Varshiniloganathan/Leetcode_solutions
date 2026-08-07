7d5d95bd554da3fd0b094671d1606e3e6db79ccc
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-08-07 21:26:30 +0530

    Time: 42 ms (5.37%) | Memory: 60.5 MB (5.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,23 +1,45 @@
 class Solution {
-    public int dfs(int row, int col, char[][] grid, Set<String> visited) {
-       
-        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) {
-            return 0;
-        }
-         String pos = row + " " + col;
+    public int bfs(int row, int col, char[][] grid, Set<String> visited) {
 
-        if (grid[row][col] == '0')
-            return 0;
-        if (visited.contains(pos))
-            return 0;
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
 
+        String pos = row + " " + col;
         visited.add(pos);
-        dfs(row + 1, col, grid, visited);
-        dfs(row - 1, col, grid, visited);
-        dfs(row, col + 1, grid, visited);
-        dfs(row, col - 1, grid, visited);
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{row,col});
+        while(!q.isEmpty()){
+            int[] point = q.poll();
+            int r = point[0];
+            int c = point[1];
+            for(int[] dir : directions) {
+                int nr = r + dir[0], nc = c+dir[1];
+                if(nr>=0 && nc>=0 &&nr<grid.length && nc<grid[0].length && grid[nr][nc] == '1' && !visited.contains(nr + " " + nc)) {
+                    
+                    bfs(nr, nc, grid, visited);
+                    
+                }
+            }
+
+        }
 
         return 1;
+        // if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) {
+        //     return 0;
+        // }
+        //  String pos = row + " " + col;
+
+        // if (grid[row][col] == '0')
+        //     return 0;
+        // if (visited.contains(pos))
+        //     return 0;
+
+        // visited.add(pos);
+        // dfs(row + 1, col, grid, visited);
+        // dfs(row - 1, col, grid, visited);
+        // dfs(row, col + 1, grid, visited);
+        // dfs(row, col - 1, grid, visited);
+
+        // return 1;
 
     }
 
@@ -27,10 +49,11 @@ class Solution {
         // int[][] visited = new int[m][n];
         Set<String> visited = new HashSet<>();
         int ans = 0;
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] != '0') {
-                    ans += dfs(i, j, grid, visited);
+                if (grid[i][j] != '0' && !visited.contains(i+" "+j)) {
+                    ans += bfs(i, j, grid, visited);
                 }
             }
         }
