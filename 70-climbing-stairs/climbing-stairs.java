badb4103d733a8af1182cd88f41d9fe7a916a610
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-08-27 18:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int recur(int n, int[] dp){
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        if(n==0 || n==1){
-            return 1;
-        }
-        dp[n] = recur(n-2,dp)+recur(n-1,dp);
-        return dp[n];
-    }
+
+    // public int recur(int n, Map<Integer,Integer> hmap){
+
+    // }
+    HashMap<Integer,Integer> map = new HashMap<>();
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return recur(n,dp);
+        
+        if(map.containsKey(n)) return map.get(n);
+        if(n==1 || n==2) return n;
+        int res = climbStairs(n-1) + climbStairs(n-2);
+        map.put(n,res);
+
+        return map.get(n);
 
         
     }
