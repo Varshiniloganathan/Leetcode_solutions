61927887257e3e289b3ada5e6a1bb2b10955f5c1
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-09-01 14:15:15 +0530

    Time: 2 ms (75.73%) | Memory: 47.6 MB (75.07%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,48 +1,51 @@
 class Solution {
-    public void bfs(int r, int c, char[][] board) {
-        int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-        board[r][c] = '#';
-        for(int[] dir : directions){
-            int nr = r + dir[0];
-            int nc = c + dir[1];
 
-            if(nr>=0 && nr<board.length && nc>=0 && nc<board[0].length && board[nr][nc] == 'O'){
-                bfs(nr,nc,board);
+    public void bfs(int i, int j, char[][] board){
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        board[i][j] = '#';
+            for(int[] dir: directions){
+                int nr = i+dir[0];
+                int nc = j+dir[1];
+                if(nr>=0 && nr<board.length && nc>=0 && nc<board[0].length && board[nr][nc] == 'O'){
+                    bfs(nr,nc,board);
+                }
             }
         }
-        
-       
-
-    }
-
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
+        int r = board.length;
+        int c = board[0].length;
 
-        for(int r=0;r<m;r++){
-            if(board[r][0] == 'O'){
-                bfs(r,0,board);
+        for(int i=0;i<r;i++){
+            if(board[i][0] == 'O'){
+                bfs(i,0,board);
             }
-
-            if(board[r][n-1] == 'O'){
-                bfs(r,n-1,board);
+            if(board[i][c-1] == 'O'){
+                bfs(i,c-1,board);
             }
         }
+        for(int i=0;i<c;i++){
+            if(board[0][i] == 'O'){
+                bfs(0,i,board);
+            }
+            if(board[r-1][i] == 'O'){
+                bfs(r-1,i,board);
+            }
+        }
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+                if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+
         
-        for(int c=0;c<n;c++){
-            if(board[0][c] == 'O'){
-                bfs(0,c,board);
-            }
-            if(board[m-1][c]== 'O'){
-                bfs(m-1,c,board);
-            }
-        }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                if(board[i][j] == '#') board[i][j] = 'O';
-            }
-        }
+
+        
     }
 }
