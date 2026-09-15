8f0a378537d7278c8ebcb91286ccc5318344ce3b
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-09-15 14:01:51 +0530

    Time: 18 ms (96.66%) | Memory: 45.5 MB (83.90%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.java
+++ 1187-print-foobar-alternately/print-foobar-alternately.java
@@ -1,37 +1,44 @@
 class FooBar {
     private int n;
-    int count = 1;
+    // int count = 1;
+    Semaphore s1 = new Semaphore(1);
+    Semaphore s2 = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
 
     }
 
-    public synchronized void foo(Runnable printFoo) throws InterruptedException {
+    public void foo(Runnable printFoo) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
             
         	// printFoo.run() outputs "foo". Do not change or remove this line.
-            while(count%2==0){
-                wait();
-            }
+            // while(count%2==0){
+            //     wait();
+            // }
+            s1.acquire();
         	printFoo.run();
-            count++;
-            notifyAll();
+            s2.release();
+
+            // count++;
+            // notifyAll();
         }
     }
 
-    public synchronized void bar(Runnable printBar) throws InterruptedException {
+    public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
             
             // printBar.run() outputs "bar". Do not change or remove this line.
-            while(count%2!=0){
-                wait();
-            }
+            // while(count%2!=0){
+            //     wait();
+            // }
+            s2.acquire();
         	printBar.run();
-            count++;
-            notifyAll();
+            s1.release();
+            // count++;
+            // notifyAll();
         }
     }
 }
