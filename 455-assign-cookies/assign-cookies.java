7c3fe8645cec5301119a164d39eb9a2d7da02186
Varshini <125352903+Varshiniloganathan@users.noreply.github.com>
2026-09-25 19:30:47 +0530

    Time: 14 ms (92.77%) | Memory: 51.5 MB (56.02%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,34 +1,16 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        if(s.length==0) return 0;
         Arrays.sort(g);
         Arrays.sort(s);
-        /*int cnt=0;
-        int n = Math.min(g.length,s.length);
-        for(int i=0;i<n;i++){
-            while(j<s.length){
-                if(g[i]<=s[j]){
-                   cnt++;
-                   s[j]=-1;
-                   break;
-                }
-                else{
-                    j++;
-                   
-                }
-                
-
-            }
-            
-            
-        }
-        return cnt;*/
         int i=0,j=0;
-        while(i<g.length&&j<s.length){
-            if(s[j]>=g[i]){
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
                 i++;
+                j++;
             }
-            j++;
+            else{
+                j++;
+            }
         }
         return i;
         
